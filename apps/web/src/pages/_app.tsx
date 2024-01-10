40867d7be36537710fc78b70909df76b25e6a716
Tim Raderschad <tim.raderschad@gmail.com>
2024-01-10 12:55:29 +0100

    feat(web): add posthog

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -13,10 +13,22 @@ import { AbbyProvider, withAbby, AbbyDevtools } from "lib/abby";
 import { useRouter } from "next/router";
 import { TooltipProvider } from "components/Tooltip";
 import "@fontsource/martian-mono/600.css";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
 import "../styles/shadcn.css";
 import "@code-hike/mdx/dist/index.css";
 import PlausibleProvider from "next-plausible";
+
+if (typeof window !== "undefined" && env.NEXT_PUBLIC_POSTHOG_KEY) {
+  // checks that we are client-side
+  posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
+    },
+  });
+}
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -38,52 +50,54 @@ const MyApp: AppType<{ session: Session | null }> = ({
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page);
   return (
-    <PlausibleProvider domain="tryabby.com">
-      <AbbyProvider initialData={__ABBY_PROJECT_DATA__}>
-        {/* we render different devtools on the landing page */}
-        {router.asPath !== "/" && <AbbyDevtools />}
-        <ThemeProvider attribute="class" defaultTheme="dark">
-          <TooltipProvider>
-            <SessionProvider session={session}>
-              <PlausibleProvider
-                domain={env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN ?? ""}
-              >
-                <main className={`font-sans`}>
-                  <DefaultSeo
-                    defaultTitle="Abby - Open Source A/B Testing & Feature Flags"
-                    titleTemplate="%s"
-                    description={seoDescription}
-                    canonical={currentPageUrl}
-                    openGraph={{
-                      url: currentPageUrl,
-                      title: "Abby",
-                      type: "website",
-                      description: seoDescription,
-                      images: [
-                        {
-                          url: `${
-                            process.env.VERCEL_URL
-                              ? `https://${process.env.VERCEL_URL}`
-                              : "https://www.tryabby.com"
-                          }/og.png`,
-                          width: 1200,
-                          height: 630,
-                          alt: "Abby",
-                          type: "image/png",
-                        },
-                      ],
-                      siteName: "Abby",
-                    }}
-                  />
-                  <Toaster />
-                  {getLayout(<Component {...pageProps} />)}
-                </main>
-              </PlausibleProvider>
-            </SessionProvider>
-          </TooltipProvider>
-        </ThemeProvider>
-      </AbbyProvider>
-    </PlausibleProvider>
+    <PostHogProvider client={posthog}>
+      <PlausibleProvider domain="tryabby.com">
+        <AbbyProvider initialData={__ABBY_PROJECT_DATA__}>
+          {/* we render different devtools on the landing page */}
+          {router.asPath !== "/" && <AbbyDevtools />}
+          <ThemeProvider attribute="class" defaultTheme="dark">
+            <TooltipProvider>
+              <SessionProvider session={session}>
+                <PlausibleProvider
+                  domain={env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN ?? ""}
+                >
+                  <main className={`font-sans`}>
+                    <DefaultSeo
+                      defaultTitle="Abby - Open Source A/B Testing & Feature Flags"
+                      titleTemplate="%s"
+                      description={seoDescription}
+                      canonical={currentPageUrl}
+                      openGraph={{
+                        url: currentPageUrl,
+                        title: "Abby",
+                        type: "website",
+                        description: seoDescription,
+                        images: [
+                          {
+                            url: `${
+                              process.env.VERCEL_URL
+                                ? `https://${process.env.VERCEL_URL}`
+                                : "https://www.tryabby.com"
+                            }/og.png`,
+                            width: 1200,
+                            height: 630,
+                            alt: "Abby",
+                            type: "image/png",
+                          },
+                        ],
+                        siteName: "Abby",
+                      }}
+                    />
+                    <Toaster />
+                    {getLayout(<Component {...pageProps} />)}
+                  </main>
+                </PlausibleProvider>
+              </SessionProvider>
+            </TooltipProvider>
+          </ThemeProvider>
+        </AbbyProvider>
+      </PlausibleProvider>
+    </PostHogProvider>
   );
 };
 
